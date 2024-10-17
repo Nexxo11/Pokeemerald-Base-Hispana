b80c9c8af984f70248023ab813aa400d185426a8
Nexxo17 <gerez.angel1899@gmail.com>
2024-10-17 03:31:50 -0300

    MAPSEC_ROUTE_120 - Deleteao

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -103,7 +103,6 @@ gStdScripts::
 	.4byte Std_MsgboxPokenav           @ MSGBOX_POKENAV
 gStdScripts_End::
 
-	.include "data/maps/Route120/scripts.inc"
 	.include "data/maps/Route121/scripts.inc"
 	.include "data/maps/Route122/scripts.inc"
 	.include "data/maps/Route123/scripts.inc"
--- include/constants/region_map_sections.h
+++ include/constants/region_map_sections.h
@@ -35,8 +35,8 @@
 #define MAPSEC_ROUTE_116                    0x1F //Eliminado
 #define MAPSEC_ROUTE_117                    0x20 //Eliminado
 #define MAPSEC_ROUTE_118                    0x21 //Eliminado
-#define MAPSEC_ROUTE_119                    0x22 //
-#define MAPSEC_ROUTE_120                    0x23
+#define MAPSEC_ROUTE_119                    0x22 //Eliminado
+#define MAPSEC_ROUTE_120                    0x23 //
 #define MAPSEC_ROUTE_121                    0x24
 #define MAPSEC_ROUTE_122                    0x25
 #define MAPSEC_ROUTE_123                    0x26
