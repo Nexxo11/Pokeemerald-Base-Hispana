983cb0535fbb65b40b686da5e3e1b5f07bc3510a
Nexxo17 <gerez.angel1899@gmail.com>
2024-10-16 22:46:48 -0300

    MAPSEC_ROUTE_108 - Deleteao

--- include/constants/region_map_sections.h
+++ include/constants/region_map_sections.h
@@ -23,8 +23,8 @@
 #define MAPSEC_ROUTE_104                    0x13 //Eliminado
 #define MAPSEC_ROUTE_105                    0x14 //Eliminado
 #define MAPSEC_ROUTE_106                    0x15 //Eliminado
-#define MAPSEC_ROUTE_107                    0x16 //
-#define MAPSEC_ROUTE_108                    0x17
+#define MAPSEC_ROUTE_107                    0x16 //Eliminado
+#define MAPSEC_ROUTE_108                    0x17 //
 #define MAPSEC_ROUTE_109                    0x18
 #define MAPSEC_ROUTE_110                    0x19
 #define MAPSEC_ROUTE_111                    0x1A
