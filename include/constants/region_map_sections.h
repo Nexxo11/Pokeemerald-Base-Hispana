34504af6409ca2fdc6e52a8ae84ff26a40812280
Nexxo17 <gerez.angel1899@gmail.com>
2024-10-17 23:22:19 -0300

    MAPSEC_UNDERWATER_SEAFLOOR_CAVERN - Deleteao

--- include/constants/region_map_sections.h
+++ include/constants/region_map_sections.h
@@ -69,8 +69,8 @@
 #define MAPSEC_MT_PYRE                      0x41 //Eliminado
 #define MAPSEC_AQUA_HIDEOUT_OLD             0x42 //Eliminado
 #define MAPSEC_SHOAL_CAVE                   0x43 //Eliminado
-#define MAPSEC_SEAFLOOR_CAVERN              0x44 //
-#define MAPSEC_UNDERWATER_SEAFLOOR_CAVERN   0x45
+#define MAPSEC_SEAFLOOR_CAVERN              0x44 //Eliminado
+#define MAPSEC_UNDERWATER_SEAFLOOR_CAVERN   0x45 //
 #define MAPSEC_VICTORY_ROAD                 0x46
 #define MAPSEC_MIRAGE_ISLAND                0x47 //Eliminado
 #define MAPSEC_CAVE_OF_ORIGIN               0x48
